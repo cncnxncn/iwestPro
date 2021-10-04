69fa63b77af9b2f4b3663cc89b56bf853d3abb60
Atilen <dkxlffps@naver.com>
2021-10-04 21:13:34 +0900

    사이드바 제거 및 동영상 컨트롤러 수정/추가

--- js/video.js
+++ js/video.js
@@ -72,10 +72,16 @@ function loadVideo(_sourceNm, lastIndex) {
       playToggle: false,
       pictureInPictureToggle: false,
     },
+    playbackRates: [1, 1.25, 1.5],
   })
 
   videojs.getPlayer("player").ready(function() {
     player = this;
+
+    this.on("ended", () => {
+      const $el = document.getElementById('playToggle-video').getElementsByTagName('img')[0];
+      $el.setAttribute('src', '../../static/image/video-playToggle-btn.png');
+    });
 
     addNavButton();
   });
@@ -84,15 +90,18 @@ function loadVideo(_sourceNm, lastIndex) {
 function addNavButton() {
 
   // before부터 넣어야 next가 오른쪽
-  createVideoBtn('before');
-  createVideoBtn('next');
+  // createVideoBtn('before');
+  // createVideoBtn('next');
+
+  createVideoBtn('playToggle');
   createVideoBtn('dbback');
 
-  createSideBar();
+  let $el = document.getElementsByClassName('vjs-volume-panel')[0];
+  video.getControlBar().insertBefore($el, video.getInsertBeforeNode());
 }
 
 const displayNone = 'display:none;';
-const defaultStyle = 'width: 25px; margin-top: 2px; cursor: pointer;';
+const defaultStyle = 'width: 25px; margin-top: 12px; cursor: pointer;';
 
 function createVideoBtn(type) {
   let $insertBeforeNode;
@@ -100,17 +109,31 @@ function createVideoBtn(type) {
   var $template = document.createElement('div'),
     $img = document.createElement('img');
   
+  
   $img.setAttribute('src', `../../static/image/video-${type}-btn.png`);
+
   $template.id = `${type}-video`;
   if(['next','before'].includes(type)) {
     $template.style = type == 'next' ? (hasNextVideo() ? '':displayNone) : (hasBeforeVideo() ? '':displayNone) ;
     $template.addEventListener('click', () => onClickVideoSourceChange(type == 'next'));
     $insertBeforeNode = video.getInsertBeforeNode();
+  } else if(type === 'playToggle') {
+    if(player.paused()) $img.setAttribute('src', '../../static/image/video-playToggle-pause-btn.png');
+    else $img.setAttribute('src', '../../static/image/video-playToggle-btn.png');
+    $template.addEventListener('click' , onClickPlayToggle);
+    $insertBeforeNode = document.getElementsByClassName('vjs-progress-control')[0];
   } else {
     $template.addEventListener('click', () => onClickPlayBack());
-    $insertBeforeNode = document.getElementsByClassName('vjs-progress-control')[0];
+    $insertBeforeNode = video.getInsertBeforeNode(); // document.getElementsByClassName('vjs-progress-control')[0];
   }
-  $img.style = defaultStyle;
+
+  if(type === 'playToggle') {
+    $img.style = 'width: 35px; margin-top: 7px; cursor: pointer;';
+  } else if (type === 'dbback') {
+    $img.style = 'width: 40px; margin-top: 14px; cursor: pointer; padding: 0px 7.5px 0 7.5px;'
+  } else {
+    $img.style = defaultStyle;
+  }
 
   $template.append($img);
   video.getControlBar().insertBefore($template, $insertBeforeNode);
@@ -153,130 +176,13 @@ function onClickPlayBack() {
   player.currentTime(newTime);
 }
 
-const config = {
-  test: [
-    {
-      title: "Expectation",
-      list: [
-        {
-          name: "ㆍ목표알기",
-          source: "1"
-        }
-      ]
-    },
-    {
-      title: "Notion",
-      list: [
-        {
-          name: "ㆍ진단하기",
-          source: "2"
-        }
-      ]
-    },
-    {
-      title: "Experience",
-      list: [
-        {
-          name: "1. SLP 기동",
-          source: "",
-        },
-        {
-          name: "2. 순환수 펌프 기동",
-          source: "",
-        },
-        {
-          name: "3. 해수 냉각수 펌프 기동",
-          source: "",
-        },
-        {
-          name: "4. WBVP 기동",
-          source: "",
-        },
-      ]
-    }
-  ]
-}
-
-function createSideBar() {
-  let data = config["test"];
-  var $layer = document.createElement('div'),
-    $template = document.createElement('div'),
-    $content = document.createElement('ul'),
-    $controlBtn = document.createElement('div'),
-    $controlBtnTitle = document.createElement('p'),
-    $controlBtnArrow = document.createElement('img');
-
-  // class binding
-  $layer.className = 'sideBarLayer';
-  $template.className = 'template';
-  $content.className = 'content';
-  $controlBtn.className = 'toggle';
-  $controlBtnTitle.className = 'title';
-  $controlBtnArrow.className = 'arrow';
-
-  $controlBtnArrow.src = '../../static/image/vector-arrow.png';
-
-  $controlBtnTitle.textContent = '학습목차';
-
-  for(var i = 0; i < data.length; i++) {
-    $content.append(getContent(data[i]));
+function onClickPlayToggle() {
+  const $el = document.getElementById('playToggle-video').getElementsByTagName('img')[0];
+  if(player.paused()) {
+    player.play();
+    $el.setAttribute('src', '../../static/image/video-playToggle-pause-btn.png')
+  } else {
+    player.pause();
+    $el.setAttribute('src', '../../static/image/video-playToggle-btn.png')
   }
-
-  $controlBtn.addEventListener('click', function() {
-    if(!this.classList.contains('on')) {
-      $layer.classList.add('active');
-    } else {
-      $layer.classList.remove('active');
-    }
-    this.classList.toggle('on');
-  })
-
-  $template.append($content);
-
-  $controlBtn.append($controlBtnTitle);
-  $controlBtn.append($controlBtnArrow);
-
-  $template.append($controlBtn);
-
-  $layer.append($template);
-
-  document.body.appendChild($layer);
-}
-
-var getContent = (_data) => {
-  var $el = document.createElement('li');
-    $title = document.createElement('div'),
-    $titleText = document.createElement('p'),
-    $content = document.createElement('div'),
-    $contentUl = document.createElement('ul');
-
-
-    let [em,text] = [_data.title.substr(0,1), _data.title.slice(1)];
-    $title.className = 'title';
-
-    $content.className = 'index';
-
-    $titleText.innerHTML = `<em>${em}</em>${text}`;
-
-    $title.append($titleText);
-
-    for(var i = 0; i < _data.list.length; i++) {
-      var $li = document.createElement('li');
-      $li.textContent = _data.list[i].name;
-      var source = _data.list[i].source;
-      console.log('index : ' , i , '\n source : ' ,source)
-      $li.addEventListener('click', () => {
-        alert(source);
-      });
-
-      $contentUl.append($li);
-    }
-
-
-    $content.appendChild($contentUl);
-
-    $el.append($title);
-    $el.append($content);
-
-    return $el;
 }
